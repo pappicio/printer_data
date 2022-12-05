7b3bca3341591b2af4fbe3592d48c30b212858d3
pappicio <52137123+pappicio@users.noreply.github.com>
2022-12-05 18:58:43 +0100

    Add files via upload

---
+++ config/scripts/generate-belt-tension-graph.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+ 
+sleep 10
+
+outdir=/home/"${USER}"/printer_data/config/belt_tension
+csvdir=/home/"${USER}"/printer_data/config/belt_tension/csv
+
+if [ ! -d "${outdir}" ]; then
+    mkdir "${outdir}"
+    chown pi:pi "${outdir}" 
+fi
+###if [ ! -d "${csvdir}" ]; then
+###    mkdir "${csvdir}"
+###    chown pi:pi "${csvdir}" 
+###fi
+
+
+if [ -e /home/"${USER}"/printer_data/config/belt_tension/4_old_resonances.png ]
+then
+    cp /home/"${USER}"/printer_data/config/belt_tension/4_old_resonances.png /home/"${USER}"/printer_data/config/belt_tension/5_old_resonances.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/belt_tension/3_old_resonances.png ]
+then
+    cp /home/"${USER}"/printer_data/config/belt_tension/3_old_resonances.png /home/"${USER}"/printer_data/config/belt_tension/4_old_resonances.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/belt_tension/2_old_resonances.png ]
+then
+    cp /home/"${USER}"/printer_data/config/belt_tension/2_old_resonances.png /home/"${USER}"/printer_data/config/belt_tension/3_old_resonances.png
+fi
+
+
+if [ -e /home/"${USER}"/printer_data/config/belt_tension/1_old_resonances.png ]
+then
+    cp /home/"${USER}"/printer_data/config/belt_tension/1_old_resonances.png /home/"${USER}"/printer_data/config/belt_tension/2_old_resonances.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/belt_tension/resonances.png ]
+then
+    cp /home/"${USER}"/printer_data/config/belt_tension/resonances.png /home/"${USER}"/printer_data/config/belt_tension/1_old_resonances.png 
+fi
+ 
+
+/home/"${USER}"/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /home/"${USER}"/printer_data/config/belt_tension/resonances.png
+###mv /tmp/raw_data_axis*.csv "${csvdir}"
+rm /tmp/raw_data_axis*.csv
---
+++ config/scripts/generate-shaper-graph-x.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NEWX=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
+
+ 
+if [ ! -d "/home/"${USER}"/printer_data/config/input_shaper" ]
+then
+    mkdir /home/"${USER}"/printer_data/config/input_shaper
+    chown "${USER}":"${USER}" /home/pi//printer_data/config/input_shaper
+fi
+
+###if [ ! -d "/home/"${USER}"/printer_data/config/input_shaper/csv" ]
+###then
+###    mkdir /home/"${USER}"/printer_data/config/input_shaper/csv
+###    chown "${USER}":"${USER}" /home/pi/printer_data/config/input_shaper/csv
+###fi
+
+if [ -e /home/"${USER}"/printer_data/config/input_shaper/4_old_resonances_x.png ]
+then
+    cp /home/"${USER}"/printer_data/config/input_shaper/4_old_resonances_x.png /home/"${USER}"/printer_data/config/input_shaper/5_old_resonances_x.png
+fi
+
+
+if [ -e /home/"${USER}"/printer_data/config/input_shaper/3_old_resonances_x.png ]
+then
+    cp /home/"${USER}"/printer_data/config/input_shaper/3_old_resonances_x.png /home/"${USER}"/printer_data/config/input_shaper/4_old_resonances_x.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/input_shaper/2_old_resonances_x.png ]
+then
+    cp /home/"${USER}"/printer_data/config/input_shaper/2_old_resonances_x.png /home/"${USER}"/printer_data/config/input_shaper/3_old_resonances_x.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/input_shaper/1_old_resonances_x.png ]
+then
+    cp /home/"${USER}"/printer_data/config/input_shaper/1_old_resonances_x.png /home/"${USER}"/printer_data/config/input_shaper/2_old_resonances_x.png
+fi
+
+if [ -e /home/"${USER}"/printer_data/config/input_shaper/resonances_x.png ]
+then
+    cp /home/"${USER}"/printer_data/config/input_shaper/resonances_x.png /home/"${USER}"/printer_data/config/input_shaper/1_old_resonances_x.png
+fi
+
+/home/"${USER}"/klipper/scripts/calibrate_shaper.py $NEWX -o /home/"${USER}"/printer_data/config/input_shaper/resonances_x.png
+
+###mv /tmp/resonances_x_*.csv /home/"${USER}"/printer_data/config/input_shaper/csv/
+rm  /tmp/resonances_x_*.csv
