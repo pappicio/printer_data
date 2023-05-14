cbdf08e6a343fe70d4e9aff75e85f2d4657b162d
pappicio <52137123+pappicio@users.noreply.github.com>
2023-05-14 09:20:28 +0200

    Add files via upload

--- config/my_ip.sh
+++ config/my_ip.sh
@@ -2,5 +2,5 @@
 
 #get IP used for defalt route (usually en0)
 IP=$(hostname  -I | cut -f1 -d' ');
-
+echo IP Stampante: $IP
 sshpass -f /home/pi/pass_file ssh root@192.168.1.169 "echo $IP > /tmp/mountd/disk1_part1/shared/ip_stampante.txt"
